adab2781bb1a2785d33fab0426293a49a12c3b43
kenji <matsunaga.kenji@canon-elec.co.jp>
2018-02-13 14:54:03 +0900

    チャプター1まで完了

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-embeddedOS
+12ステップで作る 組み込みOS自作入門 勉強用リポジトリ
 ===============
+
+# 概要
+
+[これ](https://www.amazon.co.jp/12%E3%82%B9%E3%83%86%E3%83%83%E3%83%97%E3%81%A7%E4%BD%9C%E3%82%8B%E7%B5%84%E8%BE%BC%E3%81%BFOS%E8%87%AA%E4%BD%9C%E5%85%A5%E9%96%80-%E5%9D%82%E4%BA%95-%E5%BC%98%E4%BA%AE/dp/4877832394)と[ここ](http://kozos.jp/books/makeos/)を使って組込プログラムの勉強する
---
+++ bootload/vector.c
@@ -0,0 +1,17 @@
+#include "defines.h"
+
+extern void start(void);  //スタートアップ
+
+// 割り込みベクタの設定
+// リンカスクリプトの定義により、先頭番地に配置される
+
+void (*vectors[])(void) = {
+	start, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+	NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+	NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+	NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+	NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+	NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+	NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+	NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+};
