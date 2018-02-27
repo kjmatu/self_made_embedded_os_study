33579b0ada8b2f97efae37c1c169ba036e20fc19
Matsunaga <matsunaga.kenji@canon-elec.co.jp>
2018-02-27 11:09:25 +0900

    osフォルダに流用できるbootloadプログラムをコピー

---
+++ os/lib.c
@@ -0,0 +1,174 @@
+#include "defines.h"
+#include "serial.h"
+#include "lib.h"
+
+// 1文字送信(端末変換あり)
+// シリアルへの文字出力関数
+int putc(unsigned char c)
+{
+	if (c == '\n')
+	{
+		serial_send_byte(SERIAL_DEFAULT_DEVICE, '\r');
+	}
+	return serial_send_byte(SERIAL_DEFAULT_DEVICE, c);
+}
+
+// 1文字受信
+// コンソールからの1文字受信
+unsigned char getc(void)
+{
+	unsigned char c = serial_recv_byte(SERIAL_DEFAULT_DEVICE);
+	// 改行コードの変換
+	c = ( c == '\r') ? '\n' : c;
+	// エコーバック
+	putc(c);
+	return c;
+}
+
+// 文字列送信(端末変換なし)
+int puts(unsigned char *str)
+{
+	while(*str)
+	{
+		putc(*(str++));
+	}
+	return 0;
+}
+
+// 文字列送信
+int gets(unsigned char *buf)
+{
+	int i = 0;
+	unsigned char c;
+	do {
+		c = getc();
+		if (c == '\n')
+		{
+			// 改行コードを削る
+			c = '\0';
+		}
+		buf[i++] = c;
+	} while(c);
+	return (i - 1);
+}
+
+void *memset(void *b, int c, long len)
+{
+	// ポインタ演算はvoid型のままではできないためchar型を用意する
+	char *p;
+	for (p = b; len > 0; len--)
+	{
+		// sizeof(char)づつポインタを移動させる
+		// TODO: char型(?bit)にint型(16bit)を代入しているがいいのか？
+		*(p++) = c;
+	}
+	return b;
+}
+
+void *memcpy(void *dst, const void *src, long len)
+{
+	char *d = dst;
+	const char *s = src;
+	for (; len > 0; len--)
+	{
+		*(d++) = *(s++);
+	}
+	return dst;
+}
+
+int memcmp(const void *b1, const void *b2, long len)
+{
+	const char *p1 = b1, *p2 = b2;
+	for (; len > 0; len--)
+	{
+		if (*p1 != *p2)
+			return (*p1 > *p2) ? 1 : -1;
+		p1++;
+		p2++;
+	}
+	return 0;
+}
+
+int strlen(const char *s)
+{
+	int len;
+	for (len = 0; *s; s++, len++)
+	{
+		// do nothing
+	}
+	return len;
+}
+
+char *strcpy(char *dst, const char *src)
+{
+	char *d = dst;
+	for (;; dst++, src++)
+	{
+		*dst = *src;
+		if (!*src) break;  //
+	}
+	return d;
+}
+
+int strcmp(const char *s1, const char *s2)
+{
+	while (*s1 || *s2)
+	{
+		if (*s1 != *s2)
+		{
+			return (*s1 > *s2) ? 1 : -1;
+		}
+		s1++;
+		s2++;
+	}
+	return 0;
+}
+
+int strncmp(const char *s1, const char *s2, int len)
+{
+	while ((*s1 || *s2) && (len > 0))
+	{
+		if (*s1 != *s2)
+		{
+			return (*s1 > *s2) ? 1 : -1;
+		}
+		s1++;
+		s2++;
+		len--;
+	}
+	return 0;
+}
+
+int putxval(unsigned long value, int column)
+{
+	char buf[9];  // 文字列出力用
+	char *p;
+
+	// 下の桁から処理するので、バッファの終端から格納
+	// 配列の先頭ポインタアドレス + 配列のサイズ - 1
+	// bufで確保されたメモリアドレスの終端アドレスをpに格納
+	p = buf + sizeof(buf) - 1;
+	// 終端文字
+	*(p--) = '\0';
+
+	if (!value && !column)
+	{
+		column++;
+	}
+
+	while (value || column)
+	{
+		// 16進数に変換してバッファに格納する
+		*(p--) = "0123456789abcdef"[value & 0xf];
+		// 次の桁にすすめる(4bit sift)
+		value >>= 4;  // value = value >> 4
+		// 桁数指定がある場合にはカウントする
+		if (column)
+		{
+			column--;
+		}
+	}
+	// バッファの内容を出力する
+	puts(p + 1);
+	return 0;
+}
---
+++ os/lib.h
@@ -0,0 +1,17 @@
+#ifndef _LIB_H_INCLUDED_
+#define _LIB_H_INCLUDED_
+
+int putc(unsigned char c); // 1文字送信
+unsigned char getc(void); // 1文字受信
+int puts(unsigned char *str); // コンソールから文字列送信
+int gets(unsigned char *buf); // コンソールから文字列受信
+void *memset(void *b, int c, long len);  // メモリを特定のバイトデータで埋める
+void *memcpy(void *dst, const void *src, long len); // メモリのコピー
+int memcmp(const void *b1, const void *b2, long len); // メモリの比較
+int strlen(const char *s);  // 文字列の長さ
+char *strcpy(char *dst, const char *src); // 文字列のコピー
+int strcmp(const char *s1, const char *s2); // 文字列比較
+int strncmp(const char *s1, const char *s2, int len); // 長さ指定での文字列比較
+int putxval(unsigned long value, int column);  // 数値出力
+
+#endif
