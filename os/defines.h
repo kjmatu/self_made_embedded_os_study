65ab6c44ad4063e8a893e52ab8296bb9479a351f
Matsunaga <matsunaga.kenji@canon-elec.co.jp>
2018-03-06 21:27:59 +0900

    OSで必要な型を追加

--- os/defines.h
+++ os/defines.h
@@ -10,4 +10,8 @@ typedef unsigned char uint8;
 typedef unsigned short uint16;
 typedef unsigned long uint32;
 
+typedef uint32 kz_thread_id_t;  // スレッドID
+typedef int (*kz_func_t) (int argc, char *argv[]);  // スレッドID
+typedef void (*kz_handler_t) (void);  // 割り込みハンドラの型
+
 #endif
