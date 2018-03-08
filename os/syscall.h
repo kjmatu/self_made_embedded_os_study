266225da73735213ddebaaa605f5f0caefe66cc0
Matsunaga <matsunaga.kenji@canon-elec.co.jp>
2018-03-08 16:59:44 +0900

    メッセージ送受信用のパラメータ領域を追加

--- os/syscall.h
+++ os/syscall.h
@@ -14,6 +14,8 @@ typedef enum {
     KZ_SYSCALL_TYPE_CHPRI,
     KZ_SYSCALL_TYPE_KMALLOC,
     KZ_SYSCALL_TYPE_KMFREE,
+    KZ_SYSCALL_TYPE_SEND,
+    KZ_SYSCALL_TYPE_RECV,
 } kz_syscall_type_t;
 
 // システムコール呼び出し時のパラメータ格納域の定義
@@ -58,6 +60,18 @@ typedef struct {
             char *p;
             int ret;
         } kmfree;
+        struct {
+            kz_msgbox_id_t id;
+            int size;
+            char *p;
+            int ret;
+        } send;
+        struct {
+            kz_msgbox_id_t id;
+            int *sizep;
+            char **pp;
+            kz_thread_id_t ret;
+        } recv;
     } un;  // 複数のパラメータ領域を同時に利用することはないため共用体で定義
 } kz_syscall_param_t;
 
