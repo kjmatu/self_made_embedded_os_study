99839d2b273a8d620ce739dbc8f4d42c2128eee3
Matsunaga <matsunaga.kenji@canon-elec.co.jp>
2018-03-08 14:19:24 +0900

    メモリ管理ライブラリを追加

---
+++ os/memory.h
@@ -0,0 +1,13 @@
+#ifndef _KOZOS_MEMORY_H_INCLUDED_
+#define _KOZOS_MEMORY_H_INCLUDED_
+
+// 動的メモリの初期化
+int kzmem_init(void);
+
+// 動的メモリの獲得
+void *kzmem_alloc(int size);
+
+// メモリの開放
+void kzmem_free(void *mem);
+
+#endif // !_KOZOS_MEMORY_H_INCLUDED_
