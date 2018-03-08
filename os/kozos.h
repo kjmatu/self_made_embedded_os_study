8474f596e49d4a2c3dd0e379f2a7c519fe99453c
Matsunaga <matsunaga.kenji@canon-elec.co.jp>
2018-03-08 18:04:06 +0900

    メッセージ送受信関数を定義

--- os/kozos.h
+++ os/kozos.h
@@ -32,6 +32,12 @@ void *kz_kmalloc(int size);
 // 動的メモリの解放
 int kz_kmfree(void *p);
 
+// メッセージ送信
+int kz_send(kz_msgbox_id_t id, int size, char *p);
+
+// メッセージ受信
+kz_thread_id_t kz_recv(kz_msgbox_id_t id, int *sizep, char **p);
+
 // ライブラリ関数
 // 初期スレッドを起動し、OSの動作を開始する
 void kz_start(kz_func_t func, char *name, int priority, int stacksize, int argc, char *argv[]);
@@ -44,6 +50,7 @@ void kz_syscall(kz_syscall_type_t type, kz_syscall_param_t *param);
 
 // ユーザースレッド
 // ユーザースレッドのメイン関数
-int test10_1_main(int argc, char *argv[]);
+int test11_1_main(int argc, char *argv[]);
+int test11_2_main(int argc, char *argv[]);
 
 #endif // !_KOZOS_H_INCLUDED_
